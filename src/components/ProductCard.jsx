5ad5bf692ac942172905bd9c7facc127b599db6c
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2023-05-16 00:37:23 -0400

    applied CSS styling for product cards.

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,20 @@
+export default function ProductDetails({ product }) {
+    return (
+        <div key={product._id} className="product-card">
+            <div className="product-img">
+                <img src={product.image} alt="" />
+            </div>
+            <div className="product-details">
+                <div className="product-data">
+                    <div className="product-name">
+                        <p>{product.name}</p>
+                    </div>
+                    <div className="product-info">
+                        <p className="product-price"><strong>Price: $ </strong>{product.price}</p>
+                        <p className="product-rating"><strong>Rating: </strong>{product.rating}★</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import ProductDetails from "../components/ProductDetails"
+import ProductDetails from "../components/ProductCard"
 
 export default function Home() {
     const [products, setProducts] = useState(null)
